8a51a6c07eb97a3ad5ba98df57a9842915438d0b
djordjereact <djole07mihailovic@gmail.com>
2020-05-13 18:00:16 +0200

    Finished and this project

---
+++ src/hooks-store/store.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+export const useStore = (shouldListen = true) => {
+    const setState = useState(globalState)[1];
+
+    const dispatch = async (actionIdentifier, payload) => {
+        const newState = await actions[actionIdentifier](globalState, payload);
+        globalState = { ...globalState, ...newState };
+
+        for (const listener of listeners) {
+            listener(globalState);
+        }
+    };
+
+    useEffect(() => {
+        if (shouldListen) {
+            listeners.push(setState);
+        }
+        return () => {
+            if (shouldListen) {
+                listeners = listeners.filter((li) => li !== setState);
+            }
+        };
+    }, [setState, shouldListen]);
+
+    return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+    if (initialState) {
+        globalState = { ...globalState, ...initialState };
+    }
+    actions = { ...actions, ...userActions };
+};
--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import ProductsProvider from './context/products-context';
+import configureProductStore from './hooks-store/products-store';
 
-
+configureProductStore();
 
 ReactDOM.render(
-  <ProductsProvider>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </ProductsProvider>,
+    </BrowserRouter>,
   document.getElementById('root')
 );
